09c33f36da87b06151949425d26797811d60dc50
Christian Barentine <cbarentine@gmail.com>
2015-06-16 16:08:40 -0500

    Fixed ordering of migrations

--- db/migrate/20150616210742_create_posts.rb
+++ db/migrate/20150616210742_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.title :string
-      t.body :text
-      t.user :belongs_to
+      t.string :title
+      t.text :body
+      t.belongs_to :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
